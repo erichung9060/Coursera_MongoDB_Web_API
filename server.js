4a84e068febc1821634ad56ea570f9939fd21f7f
erichung9060 <rthung96@gmail.com>
2024-11-09 03:06:18 +0800

    update

--- server.js
+++ server.js
@@ -42,11 +42,11 @@ process.on('SIGTERM', shutdownDB);
 // –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 const port = 3000;
 app.listen(port, () => {
