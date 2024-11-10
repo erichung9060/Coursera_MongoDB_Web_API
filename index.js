b78fe7a9c2625ae37361b1ed6a8c84fbd6038f1a
erichung9060 <rthung96@gmail.com>
2024-11-10 20:24:01 +0800

    final-version

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
