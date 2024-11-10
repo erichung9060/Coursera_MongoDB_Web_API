71dc11c53420c1851ffc4105902281161e93d422
erichung9060 <rthung96@gmail.com>
2024-11-10 15:31:38 +0800

    update

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
